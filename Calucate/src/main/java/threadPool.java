e05d47c42f0f57d1d1750d230e64190f45e8b46a
steven <275885026@qq.com>
2022-05-06 12:12:01 +0800

    test

--- Calucate/src/main/java/threadPool.java
+++ Calucate/src/main/java/threadPool.java
@@ -1,8 +1,10 @@
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class threadPool {
     public static void main(String[] args) {
         ExecutorService service = Executors.newWorkStealingPool();
+        System.out.println("test");
     }
 }
