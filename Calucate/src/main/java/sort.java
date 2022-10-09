cdde29b37b8573605442227596a5852cf0bd03de
steven <275885026@qq.com>
2022-10-09 15:24:52 +0800

    排序

--- Calucate/src/main/java/sort.java
+++ Calucate/src/main/java/sort.java
@@ -2,64 +2,134 @@ import java.util.*;
 
 public class sort {
 
-	public static void main(String[] args) {
-		int num[] = {45,28,80,90,50,16,100,10,33,18};
-		//mpsort(num);
-		qsort(num, 0, num.length - 1);
-	}
+    public static void main(String[] args) {
+        int count = 10000, range = 10000;
+        int num[] = createArray(count, range);
+        int num1[] = new int[count];
+        int num2[] = new int[count];
+        int num3[] = new int[count];
+        System.arraycopy(num, 0, num1, 0, count);
+        System.arraycopy(num, 0, num2, 0, count);
+        System.arraycopy(num, 0, num3, 0, count);
+        System.out.println("个数：" + count + ", 范围：" + range);
+        System.out.println(Arrays.toString(num));
 
-	// 快速排序
-	private static void qsort(int num[], int left, int right) {
-		int base = num[left];
-		int ll = left;
-		int rr = right;
-		while (ll < rr) {
-			for (;rr > ll; rr--){
-				if (num[rr] < base){
-					int t = num[rr];
-					num[rr] = num[ll];
-					num[ll] = t;
-					ll++;
-					break;
-				}
-			}
-			for (; ll < rr; ll++){
-				if (num[ll] > base){
-					int t = num[ll];
-					num[ll] = num[rr];
-					num[rr] = t;
-					rr--;
-					break;
-				}
-			}
-		}
-		System.out.println("base:" + base);
-		for (int i = 0; i < num.length; i++) {
-			System.out.print(num[i] + " ");
-		}
-		System.out.println();
-		if ((ll-1) > left) {
-			qsort(num, left, ll - 1);
-		}
-		if ((ll + 1) < right) {
-			qsort(num, ll + 1, right);
-		}
-	}
+        long s1, s2;
 
-	// 冒泡排序
-	private static void mpsort(int num[]) {
-		for (int i = 0; i < num.length - 1; i++) {
-			for (int j = i + 1; j < num.length; j++) {
-				if (num[i] > num[j]) {
-					int t = num[i];
-					num[i] = num[j];
-					num[j] = t;
-				}
-			}
-		}
-		System.out.println();
-		for (int i = 0; i < num.length; i++) {
-			System.out.print(num[i] + " ");
-		}
-	}
+        s1 = System.currentTimeMillis();
+        qsort(num1, 0, (num.length - 1));  // n*log2n   100万 116 ms
+        s2 = System.currentTimeMillis();
+        System.out.println("快排时间：" + (s2 - s1));
+        //System.out.println(Arrays.toString(num1));
+
+        s1 = System.currentTimeMillis();
+        mergeSort(num2, 0, (num2.length - 1));  // n*log2n   100万 206 ms
+        s2 = System.currentTimeMillis();
+        System.out.println("归并时间：" + (s2 - s1));
+        //System.out.println(Arrays.toString(num2));
+
+        s1 = System.currentTimeMillis();
+        mpsort(num3);  // n * n    15305 ms
+        s2 = System.currentTimeMillis();
+        System.out.println("冒泡时间：" + (s2 - s1));
+        //System.out.println(Arrays.toString(num3));
+    }
+
+    // 快速排序
+    private static void qsort(int num[], int left, int right) {
+        int base = num[left];
+        //System.out.println("base:" + base);
+        int ll = left;
+        int rr = right;
+        while (ll < rr) {
+            for (; rr > ll; rr--) {
+                if (num[rr] < base) {
+                    int t = num[rr];
+                    num[rr] = num[ll];
+                    num[ll] = t;
+                    ll++;
+                    break;
+                }
+            }
+            for (; ll < rr; ll++) {
+                if (num[ll] > base) {
+                    int t = num[ll];
+                    num[ll] = num[rr];
+                    num[rr] = t;
+                    rr--;
+                    break;
+                }
+            }
+        }
+        if ((ll - 1) > left) {
+            qsort(num, left, ll - 1);
+        }
+        if ((ll + 1) < right) {
+            qsort(num, ll + 1, right);
+        }
+    }
+
+    // 归并排序
+    private static void mergeSort(int num[], int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(num, low, mid);
+            mergeSort(num, mid + 1, high);
+            merge(num, low, mid, high);
+        }
+    }
+
+    private static void merge(int num[], int low, int mid, int high) {
+        int tempArray[] = new int[high - low + 1]; // 本段数组长度
+        int right = mid + 1;
+        int left = low;
+        int k = 0;
+        //System.out.println(String.format("low:%s,mid:%s,high:%s", low, mid, high));
+        while (right <= high && left <= mid) {
+            if (num[right] < num[left]) {
+                tempArray[k] = num[right];
+                k++;
+                right++;
+            } else {
+                tempArray[k] = num[left];
+                k++;
+                left++;
+            }
+        }
+        while (right <= high) {
+            tempArray[k] = num[right];
+            k++;
+            right++;
+        }
+        while (left <= mid) {
+            tempArray[k] = num[left];
+            k++;
+            left++;
+        }
+        for (int j = 0; j < tempArray.length; j++) {
+            num[low + j] = tempArray[j];
+        }
+        //System.out.println(Arrays.toString(num));
+    }
+
+    // 冒泡排序
+    private static void mpsort(int num[]) {
+        for (int i = 0; i < num.length - 1; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[i] > num[j]) {
+                    int t = num[i];
+                    num[i] = num[j];
+                    num[j] = t;
+                }
+            }
+        }
+    }
+
+    private static int[] createArray(int count, int range) {
+        int[] array = new int[count];
+        for (int i = 0; i < count; i++) {
+            array[i] = (int) (Math.random() * range);
+        }
+        return array;
+    }
 }
--- Calucate/src/main/java/threadPool.java
+++ Calucate/src/main/java/threadPool.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -6,5 +7,20 @@ public class threadPool {
     public static void main(String[] args) {
         ExecutorService service = Executors.newWorkStealingPool();
         System.out.println("test");
+
+        Random random = new Random();
+        int i = 0;
+        while (i < 50) {
+            String rundonStr = "";
+            while (true) {
+                rundonStr = String.valueOf(random.nextInt(1000));
+                if (rundonStr.length() == 3) {
+                    break;
+                }
+            }
+            String reqId = System.currentTimeMillis() + rundonStr;
+            System.out.println(reqId);
+            i++;
+        }
     }
 }
